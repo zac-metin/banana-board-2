94a4ba951c78a2c1d640d4f2bc4567dca85f7583
Zac Metin <zac.metin@gmail.com>
2017-06-02 11:00:07 +1200

    form should be working

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -17,13 +17,22 @@ class Form extends React.Component{
       })
     }
 
+    getTasks(state) {
+      return(state)
+    }
+
     render () {
       return (
         <div>
+        <label htmlFor="Ticket Name" />
         <input type="text" name="taskName" value={this.state.taskName} onChange={this.handleChange.bind(this)} />
+        <br /><label htmlFor="Task Description" />
         <input type="text" name="description" value={this.state.description} onChange={this.handleChange.bind(this)} />
+        <br /><label htmlFor="Banana Column" />
         <input type="text" name="completionStatus" value={this.state.completionStatus} onChange={this.handleChange.bind(this)} />
+        <br /><label htmlFor="Complexity Units" />
         <input type="text" name="complexity" value={this.state.complexity} onChange={this.handleChange.bind(this)} />
+        <br /><label htmlFor="User ID" />
         <input type="text" name="user_id" value={this.state.user_id} onChange={this.handleChange.bind(this)} />
         <button onClick={() => this.props.dispatch(getTasks(this.state))}>Create Ticket</button>
       </div>
@@ -33,7 +42,6 @@ class Form extends React.Component{
 
   }
 }
-
 
 
 Form = connect()(Form)
